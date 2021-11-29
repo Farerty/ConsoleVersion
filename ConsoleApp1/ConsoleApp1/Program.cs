20ef7731b8c00f331ef84ec801456225ced6cd98
Farerty <95239369+Farerty@users.noreply.github.com>
2021-11-29 19:43:56 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double theA = 0, theB = 0;
+            bool goToNextNum = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Write the katet a: ");
+                    theA = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Write the katet b: ");
+                    theB = Convert.ToDouble(Console.ReadLine());
+                    goToNextNum = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Number");
+                }
+            } while (goToNextNum == true);
+            double theC = 0;
+            theC = Math.Sqrt(theA * theA + theB * theB);
+            Console.WriteLine("Гипотенуза равна = {0}", theC);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double theA = 0, theB = 0, TheC = 0;
+            bool theResult = false;
+            bool goToNextNum = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Write the a: ");
+                    theA = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Write the b: ");
+                    theB = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Write the b: ");
+                    TheC = Convert.ToDouble(Console.ReadLine());
+                    goToNextNum = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Number");
+                }
+            } while (goToNextNum == true);
+            theResult = resultOfUr(theA, theB, TheC);
+            if (theResult)
+            {
+                Console.WriteLine(theResult);
+            }
+            else
+            {
+                Console.WriteLine(theResult);
+            }
+        }
+        public static bool resultOfUr(double a, double b, double c)
+        {
+            double result = b * b - 4 * a * c;
+            if (result <= 0)
+            {
+                return false;
+            }
+            else if (result > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
